1b1d90081ee19634924a50bf6b1518b2f86200b0
Javier Enrique Olazábal Silva <69092637+ItsJavito@users.noreply.github.com>
2023-07-17 21:34:49 +0000

    add sha256 scratch algorithm

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import hashlib
+import sha256 as sha
 import time
 from flask import Flask, render_template, request, jsonify
 
@@ -12,7 +12,7 @@ def upload_file():
         if file:
             # Leer el archivo y generar el hash
             content = file.read()
-            hash_value = hashlib.sha256(content).hexdigest()
+            hash_value = sha.generate_hash(content).hex()
     return render_template('index.html', hash_value=hash_value)
 
 @app.route('/check-integrity', methods=['POST'])
@@ -21,7 +21,7 @@ def check_integrity():
     hash_value = request.form['hash']
     if file:
         content = file.read()
-        calculated_hash = hashlib.sha256(content).hexdigest()
+        calculated_hash =  sha.generate_hash(content).hex()
         if calculated_hash == hash_value:
             return jsonify({'message': 'El archivo es íntegro.'})
         else:
@@ -37,14 +37,14 @@ def find_collision(start_time):
     file = request.files['file']
     if file:
         content = file.read()
-        hash1 = hashlib.sha256(content).hexdigest()
+        hash1 = sha.generate_hash(content).hex()
     else:
         return jsonify({'message': 'no se ha proporcionado un archivo.'})
     message2 = b'Alianza Lima'
-    hash2 = hashlib.sha256(message2).hexdigest()
+    hash2 =  sha.generate_hash(message2).hex()
     while hash1 != hash2:
         message2 = bytes([((x + 1) % 256) for x in message2]) # Cambiar el mensaje ligeramente
-        hash2 = hashlib.sha256(message2).hexdigest()
+        hash2 =  sha.generate_hash(message2).hex()
         #hasta un minuto buscando una colisión
         elapsed_time = time.time() - start_time
         if elapsed_time > 60:
